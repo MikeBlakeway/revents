04207ccaa1de724d4dce58fc919a445abae91950
MikeBlakeway - Insight <mike.blakeway@insight.com>
2021-02-27 20:05:43 +0000

    passing down event props

--- src/App/components/EventDashboard/EventDashboard.jsx
+++ src/App/components/EventDashboard/EventDashboard.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import EventList from '../EventList/EventList'
 import EventForm from '../EventForm/EventForm'
+import { sampleData } from '../../api/sampleData'
 
 export default function EventDashboard() {
 	return (
 		<Grid textAlign='center'>
 			<Grid.Column width={10}>
-				<EventList />
+				<EventList events={sampleData} />
 			</Grid.Column>
 			<Grid.Column width={6}>
 				<EventForm />
