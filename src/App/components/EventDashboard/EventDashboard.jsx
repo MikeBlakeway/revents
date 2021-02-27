a66eb21ced24ccc68551de8244e5a3c591f40252
MikeBlakeway - Insight <mike.blakeway@insight.com>
2021-02-27 11:28:00 +0000

    add EventForm

--- src/App/components/EventDashboard/EventDashboard.jsx
+++ src/App/components/EventDashboard/EventDashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import EventList from '../EventList/EventList'
+import EventForm from '../EventForm/EventForm'
 
 export default function EventDashboard() {
 	return (
@@ -8,7 +9,9 @@ export default function EventDashboard() {
 			<Grid.Column width={10}>
 				<EventList />
 			</Grid.Column>
-			<Grid.Column width={6}>Right Column</Grid.Column>
+			<Grid.Column width={6}>
+				<EventForm />
+			</Grid.Column>
 		</Grid>
 	)
 }
